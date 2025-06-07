f414da7c0b964bdc2e29861a122145c20668d2ac
igabakarayam <ignatiusidabagusa@gmail.com>
2025-06-07 15:17:59 +0700

    tugas 1

---
+++ Tugas_1/no3/server.py
@@ -0,0 +1,34 @@
+import socket
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    server_address = ('0.0.0.0', 32444)  
+    logging.info(f"starting up on {server_address}")
+    sock.bind(server_address)
+
+    sock.listen(1)
+    while True:
+        logging.info("waiting for a connection")
+        connection, client_address = sock.accept()
+        logging.info(f"connection from {client_address}")
+
+        while True:
+            data = connection.recv(1024)
+            if data:
+                logging.info(f"received: {data.decode()}")
+                connection.sendall(data)  # echo back
+            else:
+                break
+
+        connection.close()
+
+except Exception as ee:
+    logging.info(f"ERROR: {str(ee)}")
+finally:
+    logging.info('closing')
+    sock.close()
